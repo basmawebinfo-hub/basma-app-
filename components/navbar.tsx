febc0f9d0eed2f5a08d31144c44b55a000950698
basmawebinfo-hub <basmaweb.info@gmail.com>
2026-06-20 23:27:55 +0100

    fix(navbar): add missing LangToggle import (build error)

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
+import { LangToggle } from "@/components/lang-toggle"
 import {
   DropdownMenu,
   DropdownMenuContent,
